6c9a3c7378de474be5623ae619b03c1ef9847218
jeremy <jerelefurge@gmail.com>
2016-06-30 14:48:47 -0400

    Now saves elos to a file and can read from that file

--- readTourney.py
+++ readTourney.py
@@ -1,5 +1,4 @@
-"""
-Takes data from Challonge to calculate elos of players.
+""" Takes data from Challonge to calculate elos of players.
 
 Uses the Challonge public API and pychallonge to parse data.
 pychallonge can be found here: https://github.com/russ-/pychallonge
@@ -12,7 +11,7 @@ import challonge
 # The id is the part of the URL that comes after http://challonge.com/
 TOURNAMENT_IDS_SEPARATED_BY_COMMAS\
         = "SmashFrankys3, SFRankysFinals, sf4s, sf4finals1"
-
+DEFAULT_ELO = 1200 # Starting elo for players
 elos = {}
 import setCredentials # This is a file I made with two lines:
 # import challonge
@@ -57,27 +56,69 @@ def parseTourney(tourneyId):
     tournament = challonge.tournaments.show(tourneyId)
     print("Parsing data from " + tournament["name"] + "...")
     
+    # Go through participants
     participants = challonge.participants.index(tournament["id"])
-    playersById = {}
+    playersById = {} # Allows players to be accessed by ID
     for participant in participants:
         name = participant["display-name"].upper()
         playersById[participant["id"]] = name
         if not name in elos:
-            elos[name] = 1200
+            # Adds players to the elo records
+            elos[name] = DEFAULT_ELO
     
+    # Go through matches
     matches = challonge.matches.index(tournament["id"])
     for match in matches:
+        # We need to figure out the ID of the losing player
         if (match["winner-id"] == match["player1-id"]):
             loserId = match["player2-id"]
         else:
             loserId = match["player1-id"]
+        # Set variables for easier to read code
         winner = playersById[match["winner-id"]]
         loser = playersById[loserId]
         newElos = calculateWin(elos[winner], elos[loser] )
         elos[winner] = newElos[0]
         elos[loser] = newElos[1]
 
-tourneys = TOURNAMENT_IDS_SEPARATED_BY_COMMAS.replace(" ", "").split(",")
-for tourney in tourneys:
-    parseTourney(tourney)
-printSortedElos()
+def saveElos():
+    """
+    Save the elos to a file
+
+    Each line of the file is in the form:
+    NAME                ELO
+    For example:
+    JOHN DOE            1200
+    """
+    file = open("elos.txt", "w")
+    file.truncate()
+    for elo in elos:
+        file.write("{:20s}".format(elo) + str(elos[elo]) + "\n")
+    file.close()
+
+def readElos(filename):
+    """
+    Read pre-existing elos from a file and put them into the elo dictionary
+
+    Assumes that the file is in the format used by saveElos()
+    Args:
+        filename (str): Name/path of file beginning in current directory
+    """
+    file = open(filename)
+    for line in file:
+        temp = line.split()
+        name = ""
+        for i in range(len(temp)-1):
+            name += temp[i]
+        elos[name] = float(line.split()[-1])
+    file.close()
+
+if __name__ == "__main__":
+    filename = input("File to read elos from? Blank if none. ")
+    if (filename.strip() != ""):
+        readElos(filename)
+    tourneys = TOURNAMENT_IDS_SEPARATED_BY_COMMAS.replace(" ", "").split(",")
+    for tourney in tourneys:
+        parseTourney(tourney)
+    printSortedElos()
+    saveElos()
