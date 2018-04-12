3995a8d06dce5a0a76d16cbb037e1e9b9fde378e
Jeremy Lefurge <tcoop4@rocsoft.com>
2018-04-12 16:40:19 -0400

    Updated the way command line arguments are handled

--- calculate_elos.py
+++ calculate_elos.py
@@ -11,49 +11,40 @@ import os
 import getopt
 from elo_list import EloList
 
-USAGE = "calculate_elos.py [-h] [-t | -g | -w | -r | -e <value>] [-f]\n"\
-      + "\t-h\tDisplay this help\n"\
-      + "\t-t\tRequire a minimum amount of tournaments\n"\
-      + "\t-g\tRequire a minimum amount of games\n"\
-      + "\t-w\tRequire a minimum amount of wins\n"\
-      + "\t-r\tRequire a minimum rank\n"\
-      + "\t-e\tRequire a minimum elo\n"\
-      + "\t-f\tManually filter players"
+# Both of these are in the form (option, description)
+OPTS = [  # Options that don't require an argument
+    ("filter", "Manually filter players"),
+]
+ARGOPTS = [  # Options that require an argument
+    ("tournaments", "Require a minimum amount of tournaments"),
+    ("games", "Require a minimum amount of games"),
+    ("wins", "Require a minimum amount of wins"),
+    ("rank", "Require a minimum rank"),
+    ("elo", "Require a minimum elo"),
+]
 
-# Maybe list out the args as tuples then statically load them into these
-SHORTARGS = "hHt:g:w:r:e:f"
-LONGARGS = ["help", "tournaments=", "games=", "wins=", "rank=", "elo=", "filter"]
+# Programatically create the usage statement and appropriate options
+USAGE = "calculate_elos.py [options]\nOptions:"\
+        "\n\t-h\tDisplay this help"
+SHORTARGS = ""
+LONGARGS = []
+for opt in OPTS:
+    USAGE += "\n\t-" + opt[0][0] + "\t" + opt[1]
+    SHORTARGS += opt[0][0]
+    LONGARGS.append(opt[0])
+for opt in ARGOPTS:
+    USAGE += "\n\t-" + opt[0][0] + "\t" + opt[1]
+    SHORTARGS += opt[0][0] + ":"
+    LONGARGS.append(opt[0] + "=")
+
 
 if __name__ == "__main__":
-    minTournament = 0
-    minGames = 0
-    minWins = 0
-    minRank = 0
-    minElo = 0
-    manualFilter = False
-
     argv = sys.argv[1:]
     try:
         opts, args = getopt.getopt(argv, SHORTARGS, LONGARGS)
     except getopt.GetoptError:
         print(USAGE)
         sys.exit(2)
-    for opt, arg in opts:
-        if opt in ('-h', '-H', '--help'):
-            print(USAGE)
-            sys.exit(0)
-        if opt in ('-t', '--tournaments'):
-            minTournament = int(arg)
-        if opt in ('-g', '--games'):
-            minGames = int(arg)
-        if opt in ('-w', '--wins'):
-            minWins = int(arg)
-        if opt in ('-r', '--rank'):
-            minRank = int(arg)
-        if opt in ('-e', '--elo'):
-            minRank = int(arg)
-        if opt in ('-f', '--filter'):
-            manualFilter = True
 
     # Make sure the files to read from actually exist
     if not (os.path.isdir("obj") and os.path.exists("obj/matches.pkl")
@@ -65,24 +56,25 @@ if __name__ == "__main__":
     # Create a new EloList object and have that do the work
     elos = EloList()
     elos.calculate_elos()
-    if minTournament > 0:
-        elos.filter_by_tournaments(minTournament)
-    if minGames > 0:
-        elos.filter_by_games(minGames)
-    if minWins > 0:
-        elos.filter_by_wins(minWins)
-    if minRank > 0:
-        elos.filter_by_rank(minRank)
-    if minElo > 0:
-        elos.filter_by_elo(minElo)
-    if manualFilter:
-        elos.filter_manually()
+    for opt, arg in opts:
+        if opt.strip('-')[0] == 't':  # Tournaments
+            elos.filter_by_tournaments(arg)
+        if opt.strip('-')[0] == 'g':  # Games
+            elos.filter_by_games(arg)
+        if opt.strip('-')[0] == 'w':  # Wins
+            elos.filter_by_wins(arg)
+        if opt.strip('-')[0] == 'r':  # Rank
+            elos.filter_by_rank(arg)
+        if opt.strip('-')[0] == 'e':  # Elo
+            elos.filter_by_elo(arg)
+        if opt.strip('-')[0] == 'f':  # Manual filtering
+            elos.filter_manually()
     elos.export_spreadsheet()
     elos.save()
     elos.write_elos()
     elos.save_summaries()
     print("Elos have been saved to \"output/elos.txt\".")
-    print("A MU chart of the top 15 players has been saved to"
+    print("A MU chart of all players has been saved to"
           + " \"output/MU Chart.xlsx\".")
-    print("Top 15 summaries have been saved to \"output/summaries.txt\".")
+    print("Player summaries have been saved to \"output/summaries.txt\".")
 
